d879cf1a19d46989a47a89114a5bb3bd082fa4ba
wendizz216 <48836749+wendizz216@users.noreply.github.com>
2019-04-09 15:14:30 +0200

    Update simulation.py

--- Simulations/simulation.py
+++ Simulations/simulation.py
@@ -21,8 +21,10 @@ hcpc = [s for s in hcpc_tp_bra_sur if any(xs in s for xs in ['HND','CRI','PAN','
 
 #STEP ONE
 #############Create genotype/individual list from random sampling across different sample sizes 100 times
+x1=5
+x2=150
+sample_size = range(x1,x2+5)
 
-sample_size = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150]
 for N in sample_size:
         print N
         for ID in range(1,101): #number of iterations/sampling
